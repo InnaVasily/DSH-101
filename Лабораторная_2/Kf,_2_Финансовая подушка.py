7296aa31cd49bc55afe25f3a404a6112dbac2479
InnaVasily <139326865+InnaVasily@users.noreply.github.com>
2023-07-24 16:44:51 +0300

    Add files via upload

--- Лабораторная_2/Kf,_2_Финансовая подушка.py
+++ Лабораторная_2/Kf,_2_Финансовая подушка.py
@@ -1,13 +1,16 @@
 money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
-increase = 1.03  # Ежемесячный рост цен
+increase = 1.05  # Ежемесячный рост цен
 
 months = 0
-while money_capital > 0:
-    for months in range(months + 1):
-        money_capital -= spend - salary
-        spend *= increase
-        months += 1
+while True:
+    money_capital -= spend - salary
+    spend *= increase
+    if money_capital <= 0:
+        break
+    months += 1
+
 
 print("Количество месяцев, которое можно протянуть без долгов:", months)
+
